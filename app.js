373508e32d29636a4fee78a3a3328bb1073110c3
Mozharov Sergey <mozharov-ss@yandex.ru>
2023-02-13 14:25:20 +0300

    feature: добавить rateLimiter

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const { devDataBase } = require('./utils/constants');
 const { errorsHandler } = require('./middlewares/errorsHandler');
 const { requestLogger, errorLogger } = require('./middlewares/loggers');
 const { index } = require('./routes/index');
+const { apiLimiter } = require('./middlewares/limiter');
 
 const app = express();
 
@@ -27,7 +28,7 @@ connectToDb();
 app.use(express.json());
 app.use(requestLogger);
 
-app.use('/api', index);
+app.use('/api', apiLimiter, index);
 
 app.use(errorLogger);
 app.use(errors());
